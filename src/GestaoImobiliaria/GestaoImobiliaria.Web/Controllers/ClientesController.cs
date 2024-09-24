08f98e3b103687b89a035beb3225d220969c10bb
PabloKoerich <pablokoerich@hotmail.com>
2024-09-24 15:48:59 -0300

    *Criando Módulo Cliente. *Acerto nomes de classe.

--- src/GestaoImobiliaria/GestaoImobiliaria.Web/Controllers/ClientesController.cs
+++ src/GestaoImobiliaria/GestaoImobiliaria.Web/Controllers/ClientesController.cs
@@ -80,9 +80,6 @@ namespace GestaoImobiliaria.Web.Controllers
             return View(cliente);
         }
 
-        // POST: Clientes/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("ClienteId,Nome,Cpf,Telefone,Email")] Cliente cliente)
--- src/GestaoImobiliaria/GestaoImobiliaria.Web/Controllers/CorretoresController.cs
+++ src/GestaoImobiliaria/GestaoImobiliaria.Web/Controllers/CorretoresController.cs
@@ -48,9 +48,6 @@ namespace GestaoImobiliaria.Web.Controllers
             return View();
         }
 
-        // POST: Corretores/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("CorretorId,Nome,Cpf,Creci,Telefone,Email")] Corretores corretores)
@@ -80,9 +77,7 @@ namespace GestaoImobiliaria.Web.Controllers
             return View(corretores);
         }
 
-        // POST: Corretores/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("CorretorId,Nome,Cpf,Creci,Telefone,Email")] Corretores corretores)
