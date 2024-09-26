89a29d6334bcfbfaadf5a8beba04135d8193868d
PabloKoerich <pablokoerich@hotmail.com>
2024-09-26 18:54:30 -0300

    Finalizando modulo Clientes.

---
+++ src/GestaoImobiliaria/Favorito.Dominio.Model.cs
@@ -0,0 +1,18 @@
+﻿using Academia.Programador.Bk.Gestao.Imobiliaria.Dominio.ModuloCliente;
+
+namespace Academia.Programador.Bk.Gestao.Imobiliaria.Web;
+
+public partial class Favorito
+{
+    public int FavoritoId { get; set; }
+
+    public int ClienteId { get; set; }
+
+    public int ImovelId { get; set; }
+
+    public DateOnly DataAdicionado { get; set; }
+
+    public virtual Cliente Cliente { get; set; } = null!;
+
+    public virtual Imovel Imovel { get; set; } = null!;
+}
