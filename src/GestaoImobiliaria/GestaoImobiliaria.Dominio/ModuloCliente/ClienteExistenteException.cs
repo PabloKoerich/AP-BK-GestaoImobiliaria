9e58847ca0f61716fdd03b867dd56d67d00d2995
PabloKoerich <pablokoerich@hotmail.com>
2024-09-26 19:12:59 -0300

    correção na organização de pastas

---
+++ src/GestaoImobiliaria/GestaoImobiliaria.Dominio/ModuloCliente/ClienteExistenteException.cs
@@ -0,0 +1,8 @@
+﻿namespace GestaoImobiliaria.Dominio.ModuloCliente;
+
+public class ClienteExistenteException : Exception
+{
+    public ClienteExistenteException() : base("Cliente já cadastrado.")
+    {
+    }
+}
---
+++ src/GestaoImobiliaria/GestaoImobiliaria.Dominio/ModuloCliente/IServiceCliente.cs
@@ -0,0 +1,11 @@
+﻿namespace GestaoImobiliaria.Dominio.ModuloCliente
+{
+    public interface IServiceCliente
+    {
+        void CriarCliente(Cliente cliente);
+        List<Cliente> TragaTodosClientes();
+        void SalvarCliente(Cliente cliente);
+        Cliente TragaClientePorId(int id);
+        void Remover(int id);
+    }
+}
