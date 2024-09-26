73a22b067eddc71fac866e15473627ab2aca240e
PabloKoerich <pablokoerich@hotmail.com>
2024-09-26 18:17:50 -0300

    acertos, repositório clientes.

--- src/GestaoImobiliaria/RepositorioClientes.cs
+++ src/GestaoImobiliaria/RepositorioClientes.cs
@@ -1,7 +1,7 @@
 ﻿using Academia.Programador.Bk.Gestao.Imobiliaria.Dominio.ModuloCliente;
 using Academia.Programador.Bk.Gestao.Imobiliaria.Web;
 
-namespace Academia.Programador.Bk.Gestao.Imobiliaria.DAO.Repositorios; 
+namespace Academia.Programador.Bk.Gestao.Imobiliaria.DAO.RepositoriosEF;
 
 
 public class ClienteRepositorio : IClienteRepositorio
@@ -16,4 +16,38 @@ public class ClienteRepositorio : IClienteRepositorio
         _context.Clientes.Add(cliente);
         _context.SaveChanges();
     }
+
+    public List<Cliente> TragaTodosClientes()
+    {
+        return _context.Clientes.ToList();
+    }
+
+    public void SalvarCliente(Cliente cliente)
+    {
+        _context.Update(cliente);
+        _context.SaveChanges();
+    }
+
+    public Cliente TragaClientePorId(int? id)
+    {
+        return _context.Clientes.FirstOrDefault(cliente => cliente.ClienteId == id);
+    }
+
+    public void Remover(int id)
+    {
+        var cliente = TragaClientePorId(id);
+        _context.Clientes.Remove(cliente);
+        _context.SaveChanges();
+    }
+
+    public bool ClientePorCpf(string clienteCpf, int clienteClienteId)
+    {
+        return _context.Clientes.FirstOrDefault(cliente => string.Compare(cliente.Cpf, clienteCpf) == 0 && cliente.ClienteId != clienteClienteId) != null;
+    }
+
+    public bool ClientePorEmail(string? clienteEmail, int clienteClienteId)
+    {
+        return _context.Clientes.FirstOrDefault(cliente => string.Compare(cliente.Email, clienteEmail) == 0 && cliente.ClienteId != clienteClienteId) != null;
+    }
+}
 }
