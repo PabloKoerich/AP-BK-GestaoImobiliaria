d696fb4af38c2d176e5fca9a4af39d0f4ab34e45
PabloKoerich <pablokoerich@hotmail.com>
2024-09-29 19:35:27 -0300

    Criado IRepositório Imoveis

---
+++ src/GestaoImobiliaria/GestaoImobiliaria.Dominio/ModuloImoveis/IImoveisrepositorio.cs
@@ -0,0 +1,16 @@
+﻿using Academia.Programador.Bk.Gestao.Imobiliaria;
+using GestaoImobiliaria.Dominio.ModuloCliente;
+
+namespace GestaoImobiliaria.Dominio.ModuloImoveis
+{
+    public interface IImoveisRepositorio 
+    {
+        void CriarImoveis(Imovel imovel);
+        List<Imovel> TragaTodosImoveis();
+
+        void SalvarImovel(Imovel imovel);
+        Imovel TragaimovelPorId(int id);
+        void Remover(int id);
+        bool ImovelPorid(string imovelId, int ImovelId);
+    }
+}
