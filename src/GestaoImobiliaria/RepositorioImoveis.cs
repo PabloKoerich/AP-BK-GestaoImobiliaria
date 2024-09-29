fa513a480a2f8cd20250b460269a60b52367a5f4
PabloKoerich <pablokoerich@hotmail.com>
2024-09-29 19:09:50 -0300

    Criado repo Imoveis

--- src/GestaoImobiliaria/RepositorioImoveis.cs
+++ src/GestaoImobiliaria/RepositorioImoveis.cs
@@ -1,8 +1,19 @@
-﻿using System;
+﻿using Academia.Programador.Bk.Gestao.Imobiliaria.Dominio.ModuloCorretor;
+using Microsoft.EntityFrameworkCore;
 
-public class Class1
+namespace Academia.Programador.Bk.Gestao.Imobiliaria.DAO.Repositorios.EF.Modulo_Corretor
 {
-	public Class1()
-	{
-	}
-}
+    public class ImoveisRepositorio : IImoveisRepositorio
+    {
+        private readonly ImobiliariaDbContext _context;
+
+        public ImoveisRepositorio(ImobiliariaDbContext context)
+        {
+            _context = context;
+        }
+
+        public void CriarImoveis(Imoveis imoveis) 
+        {
+            _context.Imoveis.Add(imoveis);
+            _context.SaveChanges();
+        }
