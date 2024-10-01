2487d39ec615b11b12c1f9e5734c75d7696f0a04
PabloKoerich <pablokoerich@hotmail.com>
2024-10-01 15:25:57 -0300

    Criado imovél Exception

---
+++ src/GestaoImobiliaria/GestaoImobiliaria.Dominio/ModuloImoveis/ImoveisExistenteException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoImobiliaria.Dominio.ModuloImoveis
+{
+    public class ImoveisExistenteException : Exception
+    {
+        public ImoveisExistenteException() : base("Imóvel já Cadastrado!")
+        {
+        }
+    }
+}
+
+    
